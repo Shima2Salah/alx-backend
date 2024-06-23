affed1e0fa80acc2bb3273116b77e1cf890c711a
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-06-23 18:15:44 +0300

    mysql_data

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """helper func"""
-
-
 from typing import Tuple
+
+
 def index_range(page, page_size) -> Tuple:
     """func return start and end"""
     return ((page - 1) * page_size, page * page_size)
